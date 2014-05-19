81d81b2a88825f2c5067fee20cd34ab8f24f6d94
Edgar Azael <azael_dg@hotmail.com>
2014-05-18 20:39:43 -0500

    se agrego link del dia 19 al index

--- dist/index.php
+++ dist/index.php
@@ -27,7 +27,7 @@ $consulta=$evento->query('select id,hora,evento,ponente from programa where fech
         
         <ul class="nav nav-justified nav-pills">
           <li class="active">
-            <a href="#">
+            <a href="index.php?day=19">
               <span class="fa-stack">
                 <i class="fa fa-calendar-o fa-stack-2x"></i>
                 <span class="fa fa-stack-1x">19</span>
