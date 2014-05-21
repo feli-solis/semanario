91b2d70e8af150c4a30a4b56ab5d57c04f1a50d2
Luis Felipe Solis <solislf@gmail.com>
2014-05-20 20:55:37 -0500

    Logo de dragón en título y movida la carpeta img a dist para que sea accesible

--- dist/index.php
+++ dist/index.php
@@ -22,7 +22,11 @@ $consulta=$evento->query('select id,hora,evento,ponente from programa where fech
 
       <div class="row">
       <div class="panel panel-primary">
-        <div class="panel-heading"><h4 class="text-center">X Semana Universitaria</h4></div>
+        <div class="panel-heading">
+          <h4 class="text-center">
+            <span><img id="logo" src="img/dragon_white.svg" onerror="this.onerror=null; this.src='dragon_white.png'"></span>
+            X Semana Universitaria</h4>
+        </div>
         <div class="panel-body" id="<?php echo $day; ?>">
         
         <ul class="nav nav-justified nav-pills">
